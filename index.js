bf5fa06d85b5ef5d9531c401f380a5a9b368778e
JustinSingsanavong <justinsin1740@gmail.com>
2025-05-03 20:54:38 -0700

    [fixed] git commit hash restored.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// scroll speed function
 window.addEventListener("scroll", function () {
 document.querySelectorAll("[data-scroll-speed]").forEach(function (el) {
     const speed = parseFloat(el.getAttribute("data-scroll-speed"));
@@ -6,9 +7,20 @@ document.querySelectorAll("[data-scroll-speed]").forEach(function (el) {
 });
 });
 
+// banner shrinking function
 window.addEventListener("scroll", () => {
     const banner = document.querySelector("#banner");
     const speed = 0.5; // lower = slower
     const offset = window.scrollY * speed;
     banner.querySelector("img").style.transform = `translateY(${offset}px)`;
+});
+
+// site opacity transition function
+window.addEventListener('DOMContentLoaded', () => {
+    const content = document.querySelector('.content');
+    if (content) {
+      setTimeout(() => {
+        content.classList.add('show');
+      }, 50); // 50ms delay
+    }
 });
