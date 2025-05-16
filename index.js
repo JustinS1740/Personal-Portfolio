4fdc61ccc650c2a9d173410ae797f7465c25f0f2
Justin Singsanavong <justinsin1740@gmail.com>
2025-05-15 20:08:50 -0700

    [feat] implemented new shrinking feature where when banner shrinks, opacity for the image slowly goes to 0. And opposite for when banner is showing.
    [fixed] scroll speeds are fixed and optimized.

--- index.js
+++ index.js
@@ -2,17 +2,18 @@
 window.addEventListener("scroll", function () {
 document.querySelectorAll("[data-scroll-speed]").forEach(function (el) {
     const speed = parseFloat(el.getAttribute("data-scroll-speed"));
-    const offset = window.scrollY * (speed / 10);
-    el.style.transform = `translateY(${offset}px)`;
+    const offset = -(window.scrollY * (1.2 * speed));
+    el.style.marginTop = `${offset}px`;
 });
 });
 
-// banner shrinking function
+// banner shrinking/opacity function
 window.addEventListener("scroll", () => {
     const banner = document.querySelector("#banner");
-    const speed = 0.5; // lower = slower
-    const offset = window.scrollY * speed;
-    banner.querySelector("img").style.transform = `translateY(${offset}px)`;
+    const speed = 0.083;
+    const offset = -(window.scrollY * speed);
+    banner.style.marginTop = `${offset}px`;
+    banner.style.opacity = 1 - Math.min(window.scrollY / 300, 1);
 });
 
 // site opacity transition function
