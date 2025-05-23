47e9b4db52f05bca641522fd81e8de0929f19a81
Justin Singsanavong <justinsin1740@gmail.com>
2025-05-22 19:48:46 -0700

    [feat] title now shrinks when scrolling down page at a certain point.
    [added] footer has been added to all pages.
    
    [added] added contact title to contact page.
    
    [fixed] contact fields are now centered.
    
    [update] background color for contents have been changed for better contrast.
    
    [fixed] better transitioning when banner is shrinking.
    
    [fixed] font has been increased for about page since font was too small.

--- index.js
+++ index.js
@@ -2,18 +2,26 @@
 window.addEventListener("scroll", function () {
 document.querySelectorAll("[data-scroll-speed]").forEach(function (el) {
     const speed = parseFloat(el.getAttribute("data-scroll-speed"));
-    const offset = -(window.scrollY * (1.2 * speed));
+    const offset = -(window.scrollY * (0.6 * speed)); //1.2
     el.style.marginTop = `${offset}px`;
 });
 });
 
-// banner shrinking/opacity function
+// shrinking/opacity function
 window.addEventListener("scroll", () => {
     const banner = document.querySelector("#banner");
-    const speed = 0.35;
+    const speed = 0.35; // .35
     const offset = -(window.scrollY * speed);
     banner.style.marginTop = `${offset}px`;
-    banner.style.opacity = 1 - Math.min(window.scrollY / 300, 1);
+    banner.style.opacity = 1 - Math.min(window.scrollY / 500, 1); //300
+
+    // title shrink function
+    const title = document.querySelector("#title")
+    if (offset <= -170) {
+      title.style.fontSize = "120%";
+    } else {
+      title.style.fontSize = "200%"
+    }
 });
 
 // site opacity transition function
